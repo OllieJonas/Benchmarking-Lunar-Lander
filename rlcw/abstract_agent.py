c8d1477f5413f51a04134d50cfc3ef32a95aa8fc
Ollie <olliejonas@gmail.com>
2022-10-31 20:51:24 +0000

    Added timestep support

--- rlcw/abstract_agent.py
+++ rlcw/abstract_agent.py
@@ -9,8 +9,9 @@ def _make_env():
 
 class AbstractAgent:
 
-    def __init__(self):
-        env = _make_env()
+    def __init__(self, timesteps: int):
+        self.timesteps = timesteps
+        self.env = _make_env()
 
     def get_action(self):
         raise NotImplementedError("This hasn't been implemented yet! :(")
