0047606c90b7c8e409fff3005c0622f821c5f097
Ollie <olliejonas@gmail.com>
2022-11-04 12:27:42 +0000

    stuff

--- run.sh
+++ run.sh
@@ -5,12 +5,6 @@ IMAGE_NAME=rlcw
 CONTAINER_NAME=rlcw
 CONTAINER_BASE_PATH=""
 
-# even more boring dir setup
-mkdir -p recordings
-mkdir -p logs
-mkdir -p results
-mkdir -p policies
-
 # actually run the program
 docker build --tag=$IMAGE_NAME .
 docker run --name=$CONTAINER_NAME $IMAGE_NAME
--- scripts/startup_script.sh
+++ scripts/startup_script.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-
 # This scripts acts as a wrapper around the main program, allowing us to save any proper data before running.
+echo "Initialising display ..."
+Xvfb :1 -screen 0 1024x768x24 -ac +extension GLX +render -noreset
+export DISPLAY=:1
+echo "Display initialised"
+
+echo "Starting application ..."
+cd rlcw
+python3 -m main
