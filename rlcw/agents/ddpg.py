f42d05cb21a1ec45ed8fb9b81bdb4d6e5f39c287
Fraser <fraserd1413@gmail.com>
2022-11-19 10:35:08 +0000

    Actor network added

--- rlcw/agents/ddpg.py
+++ rlcw/agents/ddpg.py
@@ -35,6 +35,9 @@ class DdpgAgent(AbstractAgent):
     def train(self, training_context: List) -> NoReturn:
         return super().train(training_context)
 
+# Code below from
+# https://www.youtube.com/watch?v=6Yd5WnYls_Y
+
 
 class ActionNoise(object):
     def __init__(self, mu, sigma=0.15, theta=0.2, dt=1e-2, x0=None):
@@ -134,6 +137,56 @@ class CriticNetwork(nn.Module):
         state_action_value = self.q(state_action_value)
 
         return state_action_value
+
+    def save_checkpoint(self):
+        print("... saving checkpoint ...")
+        T.save(self.state_dict(), self.checkpoint_file)
+
+    def load_checkpoint(self):
+        print("... loading checkpoint ...")
+        self.load_state_dict(T.load(self.checkpoint_file))
+
+
+class ActorNetwork(nn.Module):
+    def __init__(self, alpha, input_dims, fc1_dims, fc2_dims, n_actions, name,
+                 chkpt_dir='tmp/ddpg'):
+        super(ActorNetwork, self).__init__()
+        self.intput_dims = input_dims
+        self.n_actions = n_actions
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.checkpoint_file = os.path.join(chkpt_dir, name+'_ddpg')
+        self.fc1.nn.Linear(*self.input_dims, self.fc1_dims)
+        f1 = 1 / np.sqrt(self.fc1.weight.data.size()[0])
+        T.nn.init.uniform_(self.fc1.weight.data, -f1, f1)
+        T.nn.init.uniform_(self.fc1.bias.data, -f1, f1)
+
+        self.bn1 = nn.LayerNorm(self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+        f2 = 1 / np.sqrt(self.fc2.weight.data()[0])
+        T.nn.init.uniform_(self.fc2.weight.data, -f2, f2)
+        T.nn.init.uniform_(self.fc2.bias.data, -f2, f2)
+        self.bn2 = nn.LayerNorm(self.fc2_dims)
+
+        f3 = 0.003
+        self.mu = nn.Linear(self.fc2_dims, self.n_actions)
+        T.nn.init.uniform_(self.fc3.weight.data, -f3, f3)
+        T.nn.init.uniform_(self.fc3.bias.data, -f3, f3)
+
+        self.optimser = optim.Adam(self.parameters(), lr=alpha)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+    def forward(self, state):
+        x = self.fc1(state)
+        x = self.bn1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = self.bn2(x)
+        x = F.relu(x)
+        x = T.tanh(self.mu(x))
+
+        return x
 
     def save_checkpoint(self):
         print("... saving checkpoint ...")
