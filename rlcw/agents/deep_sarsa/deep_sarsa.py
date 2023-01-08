218c77cf575856cf3db2920390b07d56299b39dc
Helen <helen2000>
2023-01-08 22:29:02 +0000

    deep sarsa seperated and working

--- rlcw/agents/deep_sarsa/deep_sarsa.py
+++ rlcw/agents/deep_sarsa/deep_sarsa.py
@@ -1,29 +1,32 @@
 """
 author: Helen
 """
+import random
 
 import numpy as np
-import torch
-import torch.nn.functional as F
-from torch import nn
 
+from typing import NoReturn, List
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch import optim
+from copy import deepcopy
+import util
 from agents.deep_sarsa.networks import StateActionNetwork
-import agents.common.utils as agent_utils
-from agents.common.policy import EpsilonGreedyPolicy
 from agents.abstract_agent import CheckpointAgent
 from replay_buffer import ReplayBuffer
 
-
 class DeepSarsaAgent(CheckpointAgent):
     """
-        Deep Sarsa agent to solve Lunar lander
+        Deep SARSA agent to solve Lunar lander
 
         This was inspired by: https://github.com/JohDonald/Deep-Q-Learning-Deep-SARSA-LunarLander-v2
     """
 
     def name(self):
         return "deep_sarsa"
-
+    
     def __init__(self, logger, config):
         super().__init__(logger, config)
 
@@ -31,38 +34,34 @@ class DeepSarsaAgent(CheckpointAgent):
         self.epsilon = config["epsilon"]
         self.gamma = config["gamma"]
         self.learning_rate = config["learning_rate"]
+        self.MSELoss_function = nn.MSELoss()
 
-        self.criterion = nn.MSELoss()
-
-        self.network = None
-        self.optim = None
-        self.policy = None
 
     def assign_env_dependent_variables(self, action_space, state_space):
         state_space = state_space.shape[0]
         action_space = action_space.n
 
-        self.network, self.optim = agent_utils.with_optim(StateActionNetwork(state_space, action_space),
-                                                          lr=self.learning_rate)
+        self.network = StateActionNetwork(state_space, action_space).to(self.device)
+        self.optimizer = optim.Adam(self.network.parameters(), lr=self.learning_rate)
 
-        self.policy = EpsilonGreedyPolicy(self.epsilon, action_space, self.device)
+    def decay_epsilon(self):
+        if self.epsilon > 0.1:
+            self.epsilon *= 0.995 # decays epsilon 
+    
+        if self.epsilon <= 0.1:
+            self.epsilon = 0.1
 
     def get_action(self, state):
-        state = torch.from_numpy(state).float()
-        network_output_to_numpy = self.network(state)
-        return self.policy.get_action(network_output_to_numpy)
 
-    def train(self, training_context: ReplayBuffer):
-        states, next_states, actions, next_actions, rewards, terminals = training_context.random_sample_sarsa(
-            self.batch_size)
-        states = torch.Tensor(states)
-        next_states = torch.Tensor(next_states)
-        actions = torch.Tensor(actions)
-        next_actions = torch.Tensor(next_actions)
-        rewards = torch.Tensor(rewards)
-        terminals = torch.Tensor(terminals)
-
-        self.update_network(states, next_states, actions, next_actions, rewards, terminals)
+        if np.random.uniform(0, 1) < self.epsilon:
+                return self.action_space.sample() 
+        else:
+                if len(state) == 2:
+                    state = state[0]
+                
+                state = torch.from_numpy(state).float()
+                network_output_to_numpy = self.network(state).data.numpy()
+                return np.argmax(network_output_to_numpy) 
 
     def update_network(self, state, next_state, action, next_action, reward, terminals):
 
@@ -71,20 +70,24 @@ class DeepSarsaAgent(CheckpointAgent):
         q_next_action = torch.gather(self.network(next_state), dim=1, index=next_action.long())
 
         qsa_next_target = reward + (self.gamma * q_next_action) * (1 - terminals)
-        q_network_loss = self.criterion(q_action, qsa_next_target.detach())
-        self.optim.zero_grad()
+        q_network_loss = self.MSELoss_function(q_action, qsa_next_target.detach())
+        self.optimizer.zero_grad()
         q_network_loss.backward()
-        self.optim.step()
-
+        self.optimizer.step()
+            
     def save(self):
         self.save_checkpoint(self.network, "StateActionNetwork")
 
     def load(self, path):
         self.load_checkpoint(self.network, path, "StateActionNetwork")
+    
+    def train(self, training_context: ReplayBuffer):
 
-    def decay_epsilon(self):
-        if self.epsilon > 0.1:
-            self.epsilon *= 0.995  # decays epsilon
-
-        if self.epsilon <= 0.1:
-            self.epsilon = 0.1
+        states, next_states, actions, next_actions, rewards, terminals = training_context.random_sample_sarsa(self.batch_size)
+        states = torch.Tensor(states)
+        next_states = torch.Tensor(next_states)
+        actions = torch.Tensor(actions)
+        next_actions = torch.Tensor(next_actions)
+        rewards = torch.Tensor(rewards)
+        terminals = torch.Tensor(terminals)
+        self.update_network(states, next_states, actions, next_actions, rewards, terminals)
